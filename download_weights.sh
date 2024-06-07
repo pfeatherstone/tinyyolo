0dde820985ebfcf07974fd442c584bddcb1ad132
pfeatherstone <pfeatherstone@pf>
2024-06-07 21:34:14 +0100

    download yolov7 weights correctly

--- download_weights.sh
+++ download_weights.sh
@@ -1,5 +1,15 @@
-wget https://pjreddie.com/media/files/yolov3-tiny.weights
-wget https://pjreddie.com/media/files/yolov3.weights
-wget https://github.com/ultralytics/yolov3/releases/download/v8/yolov3-spp.weights
-wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4.weights
-wget https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov4-tiny.weights
+
+mkdir weights
+wget https://pjreddie.com/media/files/yolov3-tiny.weights -P weights
+wget https://pjreddie.com/media/files/yolov3.weights -P weights
+wget https://github.com/ultralytics/yolov3/releases/download/v8/yolov3-spp.weights -P weights
+wget https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4.weights -P weights
+wget https://github.com/AlexeyAB/darknet/releases/download/yolov4/yolov4-tiny.weights -P weights
+git clone https://github.com/WongKinYiu/yolov7.git
+cd yolov7
+wget https://github.com/WongKinYiu/yolov7/releases/download/v0.1/yolov7.pt
+echo "import torch" >> script.py
+echo "torch.save(torch.load('yolov7.pt', map_location='cpu')['model'].float().state_dict(), '../weights/yolov7.pt')" >> script.py
+python3 script.py
+cd ..
+rm -rf yolov7

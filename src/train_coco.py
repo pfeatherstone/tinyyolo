35b2a998f620c73302a8390f2a0742de96c6053a
Your Name <you@example.com>
2024-07-16 08:49:40 +0100

    - don't do ONNX export during training. You can do that post training. Besides, this is an EXAMPLE training script. Keep it minimal. - re-enable augmentation. Still needs fine-tuning.

--- src/train_coco.py
+++ src/train_coco.py
@@ -10,7 +10,7 @@ import lightning.pytorch as pl
 from   lightning.pytorch.loggers import TensorBoardLogger
 from   lightning.pytorch.callbacks import ModelCheckpoint, LearningRateMonitor
 import matplotlib.pyplot as plt
-from   models import Yolov8, Yolov3, nms, COCO_NAMES, load_from_ultralytics, load_darknet, init_batchnorms
+from   models import *
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
 parser = argparse.ArgumentParser()
@@ -25,20 +25,6 @@ parser.add_argument("--valAnn",       type=str,   required=True,  help="Validati
 parser.add_argument("--nworkers",     type=int,   default=0,      help="Number of data workers. If 0, set to mp.cpu_count()/2")
 args = parser.parse_args()
 args.nworkers = torch.multiprocessing.cpu_count() // 2 if args.nworkers == 0 else args.nworkers
-
-class RandomResize(v2.Transform):
-    def __init__(self, min_size: int, max_size: int, max_stride: int = 32):
-        super().__init__()
-        self.min_size   = min_size
-        self.max_size   = max_size
-        self.max_stride = max_stride
-
-    def _get_params(self, flat_inputs):
-        size = ((torch.randint(self.min_size, self.max_size, (2,)) + self.max_stride - 1) // self.max_stride) * self.max_stride
-        return dict(H=size[0], W=size[1])
-
-    def _transform(self, inpt, params):
-        return self._call_kernel(v2.functional.resize, inpt, size=(params['H'], params['W']), interpolation=v2.InterpolationMode.BILINEAR, antialias=True)
 
 class CocoWrapper(torch.utils.data.Dataset):
     def __init__(self, root, annFile, transforms=[]):
@@ -101,10 +87,6 @@ class LitModule(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         self.step(batch, batch_idx, self.trainer.num_val_batches[0], is_training=False)
-    
-    def forward(self, x):
-        # For ONNX export only
-        return self.net(x)
 
     def step(self, batch, batch_idx, nbatches, is_training):
         imgs, targets = batch
@@ -149,64 +131,35 @@ class LitModule(pl.LightningModule):
                                                         max_lr=args.lr, 
                                                         total_steps=self.nsteps,
                                                         pct_start=args.nwarmup/self.nsteps)
-        return {
-            'optimizer': optimizer, 
-            'lr_scheduler': {'scheduler': scheduler, 'interval': "step", "frequency": 1}
-        }
-    
-class OnnxCheckpoint(pl.callbacks.Checkpoint):
-    def __init__(self, monitor):
-        self.monitor    = monitor
-        self.lastValue  = 100000
-        self.lastOnnx   = ""
-
-    def on_validation_end(self, trainer: pl.Trainer, lit: LitModule):
-        ckpdir = trainer.log_dir + "/checkpoints/"
-        epoch  = trainer.current_epoch
-
-        if trainer.is_global_zero:
-            if self.monitor in trainer.callback_metrics:
-                newVal = trainer.callback_metrics[self.monitor].item()
-                if newVal < self.lastValue:
-                    self.lastValue = newVal
-                    os.makedirs(ckpdir, exist_ok=True)
-                    if os.path.exists(self.lastOnnx):
-                        os.remove(self.lastOnnx)
-                    self.lastOnnx = ckpdir + "epoch={}_{}={:.4f}.onnx".format(epoch, self.monitor.replace("/", "_"), newVal)
-                    x = torch.randn(4,3,416,416)
-                    lit.to_onnx(self.lastOnnx, input_sample=(x,), opset_version=17, 
-                                input_names=['imgs'], 
-                                output_names=['preds'], 
-                                dynamic_axes={'imgs':  {0: 'B', 2: 'H', 3: 'W'},
-                                              'preds': {0: 'B', 1: 'D'}})
-                    
-        trainer.strategy.barrier()
+        return {'optimizer': optimizer, 'lr_scheduler': {'scheduler': scheduler, 'interval': "step", "frequency": 1}}
 
 torch.set_float32_matmul_precision('medium')
 
 transforms = [
-    RandomResize(320,640,32),
     v2.RandomPhotometricDistort(p=0.7),
-    v2.RandomPosterize(bits=4, p=0.7)
+    v2.RandomApply([v2.ColorJitter(brightness=(0.5,1.5),contrast=(1),saturation=(0.5,1.5))], p=0.7),
+    v2.RandomPosterize(bits=4, p=0.7),
+    v2.RandomHorizontalFlip(p=0.7),
+    v2.Resize((640,640), antialias=True)
 ]
                                    
-trainset    = CocoWrapper(args.trainRoot, args.trainAnn, transforms=[v2.Resize((640,640), antialias=True)])
+trainset    = CocoWrapper(args.trainRoot, args.trainAnn, transforms=transforms)
 valset      = CocoWrapper(args.valRoot,   args.valAnn,   transforms=[v2.Resize((416,416), antialias=True)])
 nclasses    = len(valset.names)
 trainLoader = torch.utils.data.DataLoader(trainset, batch_size=args.batchsize, shuffle=True, collate_fn=CocoCollator, num_workers=args.nworkers)
 valLoader   = torch.utils.data.DataLoader(valset, batch_size=args.batchsize, collate_fn=CocoCollator, num_workers=args.nworkers)
 nsteps      = len(trainLoader) * args.nepochs
 
-net = Yolov8('n', nclasses)
+net = Yolov8('m', nclasses)
 init_batchnorms(net)
 net = LitModule(net, nsteps)
 
 trainer = pl.Trainer(max_epochs=args.nepochs,
                      accelerator='gpu',
+                     devices=[1],
                      num_sanity_val_steps=0,
                      logger=TensorBoardLogger(save_dir="../runs", flush_secs=10),
                      callbacks= [LearningRateMonitor(logging_interval='step', log_momentum=True),
-                                 OnnxCheckpoint(monitor='loss/sum/val_epoch'),
                                  ModelCheckpoint(filename='epoch_{epoch}-loss_{loss/sum/val_epoch}',
                                                  monitor='loss/sum/val_epoch', 
                                                  auto_insert_metric_name=False)])
